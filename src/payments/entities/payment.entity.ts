5d27f11401522a0180464e705ee535222b3f22e7
sheilla Mungai <mungaisheilla19@gmail.com>
2026-09-22 20:28:39 +0300

    created entity

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -1,1 +1,10 @@
-export class Payment {}
+import { Column, Entity } from 'typeorm';
+
+@Entity()
+export class Payment {
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price!: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totaal!: number;
+}
